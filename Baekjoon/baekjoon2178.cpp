68a76035a41b2944aeae34f2bae39248a31cd2fe
micMalloc <hesue615@naver.com>
2017-07-21 18:12:21 +0900

    Baekjoon 2178
    Breadth First Search

---
+++ Baekjoon/baekjoon2178.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+int N, M;
+int map[100][100]={0,};
+const int dx[]={-1,1,0,0};
+const int dy[]={0,0,1,-1};
+void bfs(int x, int y);
+
+int main() {
+	scanf("%d %d",&N,&M);
+	for(int i = 0 ; i <N ; i++){
+		for(int j = 0 ; j < M ; j++){
+			scanf("%1d",&map[i][j]);
+		}
+	}
+
+	map[0][0] = 2;
+	bfs(0,0);
+
+	printf("%d\n",map[N-1][M-1]-1);
+
+	return 0;
+}
+
+void bfs(int x, int y){
+	queue<pair<int, int> > q;
+	q.push(make_pair(x,y));
+
+	while(!q.empty()){
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		for(int k = 0 ; k < 4; k++){
+			int nx = x + dx[k];
+			int ny = y + dy[k];
+			if(nx < 0 || nx > N-1 || ny < 0 || ny > M-1) continue;
+			if(map[nx][ny] == 1){
+				q.push(make_pair(nx,ny));
+				map[nx][ny]=map[x][y]+1;
+			}
+		}
+	}
+}
