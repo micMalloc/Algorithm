9e0fa4169bf4796d8d8044406937dd67922c68d9
micMalloc <hesue615@naver.com>
2018-01-16 19:28:42 +0900

    In-Place Quick Sort version 2

--- Sorting/QuickSort_v2.cpp
+++ Sorting/QuickSort_v2.cpp
@@ -4,6 +4,57 @@
 using namespace std;
 
 void quick_sort (int, int);
+int partition (int, int);
+
+vector<int> list;
+
+int main (int argc, char** argv) {
+	int n, key;
+
+	scanf("%d", &n);
+	for (int i = 0; i < n; ++ i) {
+		scanf("%d", &key);
+		list.push_back(key);
+	}
+	
+	quick_sort(0, n - 1);
+	for (int i = 0; i < n; ++ i) {
+		printf("%d\n", list[i]);
+	}
+
+	return 0;
+}
+
+void quick_sort (int start, int end) {
+	int pivot;
+
+	if (start < end) {
+		pivot = partition(start, end);
+		quick_sort(start, pivot - 1);
+		quick_sort(pivot + 1, end);
+	}
+}
+
+int partition (int start, int end) {
+	int pivot = end;
+	int left = start, right = end;
+
+	while (left < right) {
+		while (list[left] < list[pivot] && left < right) {
+			left ++;
+		}
+		while (list[right] >= list[pivot] && left < right) {
+			right --;
+		}
+		if (left < right) {
+			swap(list[left], list[right]);
+		}
+	} swap(list[pivot], list[right]);
+	return right;
+}
+
+/*
+void quick_sort (int, int);
 
 vector<int> list;
 
@@ -47,4 +98,4 @@ void quick_sort (int start, int end) {
 	if (right < end) {
 		quick_sort(right + 1, end);
 	}
-}
+} */
