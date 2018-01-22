ecf62c84be23e1b1a287c86b72660d3af4f0b296
micMalloc <hesue615@naver.com>
2018-01-22 23:46:43 +0900

    Using Adjacent List

--- Judge/baekjoon11657.cpp
+++ Judge/baekjoon11657.cpp
@@ -1,38 +1,79 @@
 #include <cstdio>
 #include <vector>
 #include <utility>
-#include <algorithm>
+#define INF 1e9
 using namespace std;
-typedef pair<int, int> P;
-const int INF = 1000000000;
-int main(){
-    int N, M, dist[500];
-    scanf("%d %d", &N, &M);
-    vector<P> adj[500];
-    for(int i=0; i<M; i++){
-        int A, B, C;
-        scanf("%d %d %d", &A, &B, &C);
-        adj[A-1].push_back(P(B-1, C));
-    }
-    bool minusCycle = false;
-    fill(dist, dist+N, INF);
-    dist[0] = 0;
-    for(int i=0; i<N; i++){ 
-        for(int j=0; j<N; j++){
-            for(auto &p: adj[j]){
-                int next = p.first, d = p.second;
-                if(dist[j] != INF && dist[next] > dist[j] + d){
-                    dist[next] = dist[j] + d;
-                    if(i == N-1) minusCycle = true;
-                }
-            }
-        }
-    }
-    if(minusCycle) {
-		puts("-1");
+
+bool Bellman_Ford (int, int);
+void initSingleSource (int, int);
+bool relaxation(int, int, int);
+
+typedef pair<int, int> Pair;
+
+vector<vector<Pair> > adj;
+vector<int> d;
+
+int main (int argc, char** argv) {
+	int n, m;
+	int a, b, c;
+
+	scanf("%d %d", &n, &m);
+	adj.resize(n + 1);
+	d.resize(n + 1);
+
+	while (m --) {
+		scanf("%d %d %d", &a, &b, &c);
+		adj[a].push_back(make_pair(b, c));
 	}
-    else{
-        for(int i=1; i<N; i++)
-            printf("%d\n", dist[i]!=INF ? dist[i] : -1);
-    }
+
+	if (Bellman_Ford(1, n)) {
+		for (int i = 2; i <= n; ++ i) {
+			if (d[i] == INF) printf("-1\n");
+			else printf("%d\n", d[i]);
+		}
+	} else {
+		printf("-1\n");
+	}
+
+	return 0;
+}
+
+bool Bellman_Ford (int source, int n) {
+	int u, v, w;
+	
+	initSingleSource(source, n);
+	for (int i = 0; i < n - 1; ++ i) {
+		for (int j = 1; j <= n; ++ j) {
+			for (auto p : adj[j]) {
+				relaxation(j, p.first, p.second);
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; ++ i) {
+		for (auto p : adj[i]) {
+			if (relaxation(i, p.first, p.second)) {
+				return false;
+			}
+		}
+	} return true;
+}
+
+void initSingleSource (int source, int n) {
+	for (int i = 1; i <= n; ++ i) {
+		if (i == source) {
+			d[i] = 0;
+		} else {
+			d[i] = INF;
+		}
+	}
+}
+
+bool relaxation (int u, int v, int w) {
+	bool isRelax = false;
+	
+	if (d[v] > d[u] + w) {
+		d[v] = d[u] + w;
+		isRelax = true;
+	} return isRelax;
 }
