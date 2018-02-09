1fbb4f73febcfcef2199b6b401bacfddbb709f89
micMalloc <hesue615@naver.com>
2018-02-10 03:57:21 +0900

    Using Warshall Algorithm

--- Judge/baekjoon11403.cpp
+++ Judge/baekjoon11403.cpp
@@ -1,36 +1,42 @@
-¤¼#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <cstdio>
+#define MAX  101
 using namespace std;
 
-typedef pair<int, int> Pair;
-vector<Pair> input;
-vector<Pair> ans;
+void Warshall (int);
+void print_result (int);
+
+int adj[MAX][MAX];
 
 int main (int argc, char** argv) {
 	int n;
-	int r, l, result = 0;
 
 	scanf("%d", &n);
-	for (int i = 0; i < n; ++ i) {
-		scanf("%d %d", &l, &r);
-		input.push_back(make_pair(l, r));
-	} sort(input.begin(), input.end());
-
-	ans.push_back(input[0]);
-	for (int i = 1; i < n; ++ i) {
-		if (ans[ans.size() - 1].second >= input[i].first) {
-			if (ans[ans.size() - 1].second < input[i].second) {
-				ans[ans.size() - 1].second = input[i].second;
-			}
-		} else {
-			ans.push_back(input[i]);
+	for (int i = 1; i <= n; ++ i) {
+		for (int j = 1; j <= n; ++ j) {
+			scanf("%d", &adj[i][j]);
 		}
-	}
-
-	for (int i = 0; i < ans.size(); ++ i) {
-		result += ans[i].second - ans[i].first;
-	} printf("%d\n", result);
+	} Warshall(n);
+	print_result(n);
 
 	return 0;
+}
+
+void Warshall (int n) {
+	int i, j, k;
+
+	for (int k = 0; k < n; ++ k) {
+		for (int i = 1; i <= n; ++ i) {
+			for (int j = 1; j <= n; ++ j) {
+				adj[i][j] = adj[i][j] | (adj[i][k + 1] & adj[k + 1][j]);
+			}
+		}
+	}
+}
+
+void print_result (int n) {
+	for (int i = 1; i <= n; ++ i) {
+		for (int j = 1; j <= n; ++ j)
+			printf("%d ", adj[i][j]);
+		printf("\n");
+	}
 }
