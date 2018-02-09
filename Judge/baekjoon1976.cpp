2e7b0bfdc913dd8043076fcd992b64260bb1626b
micMalloc <hesue615@naver.com>
2018-02-09 16:24:53 +0900

    Complete Problem

--- Judge/baekjoon1976.cpp
+++ Judge/baekjoon1976.cpp
@@ -1,50 +1,68 @@
 #include <cstdio>
 #include <vector>
-#define MAX 201	
+#include <algorithm>
 using namespace std;
 
-int adj[MAX][MAX];
-int n, m;
+void init (int);
+int find_set (int);
+void union_set (int, int);
 
-void warshall ();
+vector<int> p;
 
 int main (int argc, char** argv) {
-	vector<int> list;
+	int n, m;
+	int isAdj;
 
 	scanf("%d", &n);
 	scanf("%d", &m);
+	init(n);
 
-	list.resize(m);
-	
-	for (int i = 1; i <= n; ++ i)
-		for (int j = 1; j <= n; ++ j)
-			scanf("%d", &adj[i][j]);
-	for (int i = 0; i < m; ++ i)
-		scanf("%d", &list[i]);
-	warshall();
-	
-	bool isAvailable = true;
-	int u, v;
-
-	u = list[0];
-	for (int i = 1; i < m; ++ i) {
-		v = list[i];
-		if (adj[u][v] != 1)
-			isAvailable = false;
-		u = v;
+	for (int i = 1; i <= n; ++ i) {
+		for (int j = 1; j <= n; ++ j) {
+			scanf("%d", &isAdj);
+			if (isAdj == 1) {
+				union_set(i, j);
+			}
+		}
 	}
+	
+	int u, v;
+	bool isAvail = true;
+	scanf("%d", &v);
 
-	if (isAvailable) printf("YES\n");
+	u = find_set(v);
+	for (int i = 0; i < m - 1; ++ i) {
+		scanf("%d", &v);
+		if (u != find_set(v)) {
+			isAvail = false;
+		}
+	}
+	
+	if (isAvail) printf("YES\n");
 	else printf("NO\n");
 
 	return 0;
 }
 
-void warshall () {
-	int i, j, k;
+void init (int n) {
+	p.resize(n + 1);
+	for (int i = 1; i <= n; ++ i) {
+		p[i] = i;
+	}
+}
 
-	for (k = 1; k <= n; ++ k)
-		for (i = 1; i <= n; ++ i)
-			for (j = 1; j <= n; ++ j)
-				adj[i][j] = adj[i][j] | (adj[i][k] & adj[k][j]);
+int find_set (int u) {
+	if (u == p[u])
+		return u;
+	return p[u] = find_set(p[u]);
+}
+
+void union_set (int a, int b) {
+	a = find_set(a);
+	b = find_set(b);
+
+	if (a == b) return;
+
+	if (a > b) swap(a, b);
+	p[b] = a;
 }
