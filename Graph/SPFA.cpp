f12039b786162fb7e40c183445ab2e87ae859ccc
micMalloc <hesue615@naver.com>
2018-02-22 18:46:48 +0900

    Shortest Path Faster Algorithm

--- Graph/SPFA.cpp
+++ Graph/SPFA.cpp
@@ -1,39 +1,24 @@
-bool Bellman_Ford (int source, int n) {
-	int u, v, w;
-	
-	initSingleSource(source, n);
-	for (int i = 0; i < n - 1; ++ i) {
-		for (int j = 1; j <= n; ++ j) {
-			for (auto p : adj[j]) {
-				relaxation(j, p.first, p.second);
+
+void SPFA (int n, int s) {
+	queue<int> q;
+	vector<bool> isInQ(n + 1, false);
+
+	for (int i = 1; i <= n; ++ i) {
+		d[i] = INF;
+	} d[s] = 0;
+
+	q.push(s); isInQ[s] = true;
+	while (!q.empty()) {
+		int u = q.front(); q.pop(); isInQ[u] = false;
+		for (auto p : adj[u]) {
+			int v = p.first, w = p.second;
+			if (d[v] > d[u] + w) {
+				d[v] = d[u] + w;
+				if (!isInQ[v]) {
+					q.push(v);
+					isInQ[v] = true;
+				}
 			}
 		}
 	}
-
-	for (int i = 1; i <= n; ++ i) {
-		for (auto p : adj[i]) {
-			if (relaxation(i, p.first, p.second)) {
-				return false;
-			}
-		}
-	} return true;
-}
-
-void initSingleSource (int source, int n) {
-	for (int i = 1; i <= n; ++ i) {
-		if (i == source) {
-			d[i] = 0;
-		} else {
-			d[i] = INF;
-		}
-	}
-}
-
-bool relaxation (int u, int v, int w) {
-	bool isRelax = false;
-	
-	if (d[v] > d[u] + w) {
-		d[v] = d[u] + w;
-		isRelax = true;
-	} return isRelax;
 }
